38bb7ea11aefb68ac63b6486f5dd27508461aba4
fernandorossetti <fernando_r_@hotmail.com>
2023-04-25 23:57:06 -0300

    nuevos cambios

--- src/components/ItemListContainer/ItemListContainer.js
+++ src/components/ItemListContainer/ItemListContainer.js
@@ -1,8 +1,23 @@
+import { useEffect, useState } from "react";
+import ItemList from '../ItemList/ItemList';
 const ItemListContainer = ({ gretting }) => {
+    const [products, setProducts] = useState([])
+
+    useEffect (() => {
+        getProducts()
+            .then(response =>{
+                setProducts(response)
+            })
+            .catch(error => {
+                console.log(error)
+            }) 
+    }, [])
+
     return (
-        <h1>
-            {gretting}
-        </h1>
+        <div>
+        <h1> {gretting} </h1>
+        <ItemList products={products}></ItemList>
+        </div>
     )
 }
 
