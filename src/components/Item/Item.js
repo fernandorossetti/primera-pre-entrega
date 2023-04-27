bda3960b269b6114a4b38f3e2afd0b4e74497214
fernandorossetti <fernando_r_@hotmail.com>
2023-04-26 23:07:43 -0300

    Cambios de estilo y código

--- src/components/Item/Item.js
+++ src/components/Item/Item.js
@@ -1,46 +1,30 @@
-import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
 import { Link } from 'react-router-dom';
+import CardGroup from 'react-bootstrap/CardGroup';
+import { Button, Row } from 'react-bootstrap';
 
 const Item = ({id, name, price, img, stock}) => {
 
     return(
-        <Card className="text-center" style={{ width: '18rem' }} >
-        <Card.Title>{name}</Card.Title>
+      <Row className='justify-content-center align-items-center minh-100'>
+      <CardGroup style={{ width: '18rem' }}>
+        <br/>
+        <Card >
+        <br/>
         <Card.Img variant="top" src={img} alt={name} />
+        <Card.Title>{name}</Card.Title>
         <Card.Body>
           <Card.Text> Precio: ${price}</Card.Text>
           <Card.Text> Stock: ${stock}</Card.Text>
         </Card.Body>
         <Card.Footer>
-          <Link to={`/item/${id}`} className='Option'> Ver Detalle </Link>
+          <Button variant='light'><Link to={`/item/${id}`} className='Option'> Ver Detalle </Link></Button>
         </Card.Footer>
+        <br />
       </Card>
+      </CardGroup>
+      </Row>
     )
 }
 
 export default Item;
-
-/*<Card style={{ width: '18rem' }} >
-<Card.Header>
-    <h2>
-        {name}
-    </h2>
-</Card.Header>
-<Card.Img src={img} alt={name}>
-    
-</Card.Img>
-<section>
-    <p>
-      Precio: ${price} 
-    </p>
-    <p>
-      Stock Disponible: {stock}
-    </p>
-</section>
-<footer>
-    <button>
-        Ver Detalle
-    </button>
-</footer>
-</Card>*/
--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -1,23 +1,27 @@
+import { Button } from "react-bootstrap";
 import CardWidget from "../CardWidget/CardWidget";
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import { Link, NavLink } from "react-router-dom";
 
+
 const NavBar = () => {
     return (
 
-        <Navbar bg="secondary" variant="dark">
+        <Navbar bg="dark" variant="dark">
         <Container>
-          <Navbar.Brand className="navbar-center">
-            <Link to='/'>
+          <Navbar.Brand>
+            <Button variant="link">
+              <Link to='/'>
             <h3>Tecno Phone</h3>
             </Link>
+            </Button>
           </Navbar.Brand>
-          <Nav className="me-auto">
-            <NavLink to={`/category/servicios`} className={({isActive}) => isActive ? 'ActiveOption' : 'Option'}>Servicios</NavLink>
-            <NavLink to={`/category/productos`} className={({isActive}) => isActive ? 'ActiveOption' : 'Option'}>Productos</NavLink>
-            <NavLink to={`/category/contacto`} className={({isActive}) => isActive ? 'ActiveOption' : 'Option'}>Contacto</NavLink>
+          <Nav className='justify-content-center align-items-center'>
+            <Button variant="link"><NavLink to={`/category/iPhone`} className={({isActive}) => isActive ? 'ActiveOption' : 'Option'}>Iphone</NavLink></Button>{' '}
+            <Button variant="link"><NavLink to={`/category/MacBook`} className={({isActive}) => isActive ? 'ActiveOption' : 'Option'}>MacBook</NavLink></Button>{' '}
+            <Button variant="link"><NavLink to={`/category/Accesorios`} className={({isActive}) => isActive ? 'ActiveOption' : 'Option'}>Accesorios</NavLink></Button>{' '}
             <CardWidget/>
           </Nav>
         </Container>
