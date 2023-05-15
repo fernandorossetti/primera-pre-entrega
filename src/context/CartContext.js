261ac840464f86cd343517df7e59f2776af7314a
fernandorossetti <fernando_r_@hotmail.com>
2023-05-15 19:54:23 -0300

    cambio nombre carpeta

--- src/context/CartContext.js
+++ src/context/CartContext.js
@@ -1,5 +1,5 @@
 import { createContext, useState } from "react";
-//import Item from "../components/Item/Item";
+//import Item from "../components/Item/Item"; 
 
 
 export const CartContext = createContext({
